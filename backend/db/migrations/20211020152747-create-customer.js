f9e7d03892ecff43098535302d9a9c4ec2c6611b
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-20 08:53:13 -0700

    generated customer model with cart attribute holding ints (no join required)

---
+++ backend/db/migrations/20211020152747-create-customer.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Customers", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      hashedPassword: {
+        type: Sequelize.STRING,
+      },
+      cart: {
+        type: Sequelize.ARRAY(Sequelize.INTEGER),
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Customers");
+  },
+};
