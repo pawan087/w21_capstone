cf0608c4fe3ab3efcc5f4b00359388c474edcbfb
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-20 23:36:17 -0700

    onto answerLikes, once complete all read functionality will be working

--- frontend/src/components/Product/AnswersCard.js
+++ frontend/src/components/Product/AnswersCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "./ProductPage.module.css";
 
 export default function AnswersCard({ answers }) {
-  console.log(answers);
   return (
     <div>
       {answers?.map((answer) => {
         return (
           <>
             <li>{answer.User.username}</li>
+
             <li>{answer.content}</li>
 
             <br />
