1f9e58997fe4c00093b1bb0ff5197acfe3059ed0
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-10-22 10:00:39 -0700

    fixed multi cart item update quantity bug

--- frontend/src/components/Cart/Items.js
+++ frontend/src/components/Cart/Items.js
@@ -9,7 +9,6 @@ export default function Items({ shoppingCartItems }) {
 
   const user = useSelector((state) => state.session.user);
 
-  let sumQuantity = 0;
   let idToDelete1;
   let idToDelete2;
   let productId;
@@ -17,7 +16,10 @@ export default function Items({ shoppingCartItems }) {
   let arr = [];
 
   shoppingCartItems?.forEach((item1, i) => {
+    let sumQuantity = 0;
+
     let id1 = item1.product.id;
+
     for (let j = i + 1; j < shoppingCartItems.length; j++) {
       let item2 = shoppingCartItems[j];
 
@@ -28,25 +30,24 @@ export default function Items({ shoppingCartItems }) {
         idToDelete2 = item2.id;
         productId = item2.product.id;
         sumQuantity += item2.quantity + item1.quantity;
+
         arr.push({ idToDelete1, idToDelete2, productId, sumQuantity });
       }
     }
   });
 
   useEffect(() => {
-    if (arr?.length > 0) {
-      arr?.forEach(async (duplicate, i) => {
-        await dispatch(
-          consolidateCartItems({
-            idToDelete1: duplicate.idToDelete1,
-            idToDelete2: duplicate.idToDelete2,
-            sumQuantity: duplicate.sumQuantity,
-            productId: duplicate.productId,
-            userId: user.id,
-          })
-        );
-      });
-    }
+    arr?.forEach(async (duplicate, i) => {
+      await dispatch(
+        consolidateCartItems({
+          idToDelete1: duplicate.idToDelete1,
+          idToDelete2: duplicate.idToDelete2,
+          sumQuantity: duplicate.sumQuantity,
+          productId: duplicate.productId,
+          userId: user.id,
+        })
+      );
+    });
   }, [dispatch]);
 
   return (
