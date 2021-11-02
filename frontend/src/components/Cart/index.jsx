81c715b2c8e7ab00867152a9de8b342f1e8dcf76
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-11-01 17:17:35 -0700

    styled cart page

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -112,7 +112,7 @@ export default function Cart() {
   });
 
   const handleQuantityChange = (value) => {
-    console.log('Value changed to -->', value);
+    console.log("Value changed to -->", value);
   };
 
   return (
@@ -120,103 +120,105 @@ export default function Cart() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className={styles.cartOuterContainer}
+      className={styles.backContainer}
     >
-      <div className={styles.cartLeftContainer}>
-        <div className={styles.leftTopContainer}>
-          <div className={styles.homeIconContainer}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-            </svg>
-          </div>
-
-          <div className={styles.leftTopTitle}>
-            Ship To Home:{" "}
-            <span className={styles.itemCountNotBold}>2 Items</span>
-          </div>
-        </div>
-
-        <div className={styles.leftBottomContainer}>
-          <div className={styles.leftBottom1stContainer}>
-            <div className={styles.productImageContainer}>
-              <img
-                className={styles.productImage}
-                src="https://media.gamestop.com/i/gamestop/11098255_gold/iPhone-11-Pro-Max-64GB---Unlocked-gold?$pdp2x$"
-                alt="shoppingCartImage"
-              />
+      <div className={styles.cartOuterContainer}>
+        <div className={styles.cartLeftContainer}>
+          <div className={styles.leftTopContainer}>
+            <div className={styles.homeIconContainer}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+              </svg>
             </div>
 
-            <div className={styles.quantityContainer}>
-              <Select
-                placeholder={`Qty ${1}`}
-                options={options}
-                theme={theme}
-                defaultValue={options[0]}
-                onChange={(e) => handleQuantityChange(e.value)}
-              />
+            <div className={styles.leftTopTitle}>
+              Ship To Home:{" "}
+              <span className={styles.itemCountNotBold}>2 Items</span>
             </div>
           </div>
-          <div className={styles.leftBottom2ndContainer}>
-            <div className={styles.leftBottom2ndTopContainer}>
-              <div className={styles.cartProductName}>
-                Microsoft Xbox Series X Stereo Headset
+
+          <div className={styles.leftBottomContainer}>
+            <div className={styles.leftBottom1stContainer}>
+              <div className={styles.productImageContainer}>
+                <img
+                  className={styles.productImage}
+                  src="https://media.gamestop.com/i/gamestop/11098255_gold/iPhone-11-Pro-Max-64GB---Unlocked-gold?$pdp2x$"
+                  alt="shoppingCartImage"
+                />
               </div>
-              <div className={styles.cartProductBrandName}>Xbox Series X</div>
+
+              <div className={styles.quantityContainer}>
+                <Select
+                  placeholder={`Qty ${1}`}
+                  options={options}
+                  theme={theme}
+                  defaultValue={options[0]}
+                  onChange={(e) => handleQuantityChange(e.value)}
+                />
+              </div>
             </div>
-            <div className={styles.leftBottom2ndBottomContainer}>
-              <div className={styles.removeFromCartLink}>Remove</div>
+            <div className={styles.leftBottom2ndContainer}>
+              <div className={styles.leftBottom2ndTopContainer}>
+                <div className={styles.cartProductName}>
+                  Microsoft Xbox Series X Stereo Headset
+                </div>
+                <div className={styles.cartProductBrandName}>Xbox Series X</div>
+              </div>
+              <div className={styles.leftBottom2ndBottomContainer}>
+                <div className={styles.removeFromCartLink}>Remove</div>
+              </div>
             </div>
-          </div>
-          <div className={styles.leftBottom3rdContainer}>
-            <input checked className={styles.fakeRadio} type="radio" /> 
-            <div className={styles.fakeFreeShipping}>
-              FREE shipping{" "}
-              <span className={styles.shippingDetail}>on $35+ orders</span>
+            <div className={styles.leftBottom3rdContainer}>
+              <input checked className={styles.fakeRadio} type="radio" /> 
+              <div className={styles.fakeFreeShipping}>
+                FREE shipping{" "}
+                <span className={styles.shippingDetail}>on $35+ orders</span>
+              </div>
             </div>
-          </div>
-          <div className={styles.leftBottom4thContainer}>
-            <div className={styles.priceTag}>$88.99</div>
+            <div className={styles.leftBottom4thContainer}>
+              <div className={styles.priceTag}>$88.99</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.cartRightContainer}>
-        <div className={styles.right1stContainer}>
-          <div className={styles.rightTitle}>ORDER SUMMARY</div>
-        </div>
-        <div className={styles.right2ndContainer}>
-          <div className={styles.right2nd1stContainer}>
-            <div className={styles.subtotalLabel}>
-              Subtotal <span className={styles.itemCount}>(2 items)</span>
+        <div className={styles.cartRightContainer}>
+          <div className={styles.right1stContainer}>
+            <div className={styles.rightTitle}>ORDER SUMMARY</div>
+          </div>
+          <div className={styles.right2ndContainer}>
+            <div className={styles.right2nd1stContainer}>
+              <div className={styles.subtotalLabel}>
+                Subtotal <span className={styles.itemCount}>(2 items)</span>
+              </div>
+
+              <div className={styles.subtotalAmount}>$721.96</div>
             </div>
 
-            <div className={styles.subtotalAmount}>$721.96</div>
+            <div className={styles.right2nd2ndContainer}>
+              <div className={styles.shippingLabel}>Shipping & Handling</div>
+
+              <div className={styles.fakeShippingCost}>FREE</div>
+            </div>
+
+            <div className={styles.right2nd3rdContainer}>
+              <div className={styles.estimatedTaxLabel}>Estimated Tax</div>
+
+              <div className={styles.estimatedTaxAmount}>$75.80</div>
+            </div>
           </div>
+          <div className={styles.right3rdContainer}>
+            <div className={styles.estimatedTotalLabel}>Estimated Total</div>
 
-          <div className={styles.right2nd2ndContainer}>
-            <div className={styles.subtotalLabel}>Shipping & Handling</div>
-
-            <div className={styles.fakeShippingCost}>FREE</div>
+            <div className={styles.estimatedTotalAmount}>$797.78</div>
           </div>
-
-          <div className={styles.right2nd3rdContainer}>
-            <div className={styles.estimatedTaxLabel}>Estimated Tax</div>
-
-            <div className={styles.estimatedTaxAmount}>$75.80</div>
+          <div className={styles.right4thContainer}>
+            <button className={styles.button}>PROCEED TO CHECKOUT</button>
           </div>
-        </div>
-        <div className={styles.right3rdContainer}>
-          <div className={styles.estimatedTotalLabel}>Estimated Total</div>
-
-          <div className={styles.estimatedTotalAmount}>$797.78</div>
-        </div>
-        <div className={styles.right4thContainer}>
-          <button className={styles.button}>PROCEED TO CHECKOUT</button>
         </div>
       </div>
     </motion.div>
