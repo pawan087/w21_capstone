01b1d6531659d4e253da3e9e64c024dab40a6b4a
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-11-09 17:43:51 -0800

    adding validations for sign up page

--- frontend/src/components/CreateAccount/CreateAccount.jsx
+++ frontend/src/components/CreateAccount/CreateAccount.jsx
@@ -8,6 +8,170 @@ import styles from "./styles.module.css";
 
 export default function CreateAccount() {
   const [notRobot, setNotRobot] = useState(true);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
+  const [phone, setPhone] = useState("");
+  const [inputType, setInputType] = useState("password");
+  const [incorrectFormatEmail, setIncorrectFormatEmail] = useState(false);
+  const [missingFirstName, setMissingFirstName] = useState(false);
+  const [missingLastName, setMissingLastName] = useState(false);
+  const [missingEmail, setMissingEmail] = useState(false);
+  const [missingPassword, setMissingPassword] = useState(false);
+  const [missingAddress1, setMissingAddress1] = useState(false);
+  const [missingAddress2, setMissingAddress2] = useState(false);
+  const [missingPhone, setMissingPhone] = useState(false);
+  const [incorrectFormatPhone, setIncorrectFormatPhone] = useState(false);
+  const [missingUsername, setMissingUsername] = useState(false);
+
+  const showPassword = (e) => {
+    e.preventDefault();
+
+    setInputType("text");
+  };
+
+  const hidePassword = (e) => {
+    e.preventDefault();
+
+    setInputType("password");
+  };
+
+  const changeFirstName = (value) => {
+    if (missingFirstName) {
+      setMissingFirstName(false);
+    }
+
+    setFirstName(value);
+  };
+
+  const changeLastName = (value) => {
+    if (missingLastName) {
+      setMissingLastName(false);
+    }
+
+    setLastName(value);
+  };
+
+  const changeUsername = (value) => {
+    if (missingUsername) {
+      setMissingUsername(false);
+    }
+
+    setUsername(value);
+  };
+
+  const changeEmail = (value) => {
+    if (missingEmail) {
+      setMissingEmail(false);
+    }
+
+    if (incorrectFormatEmail) {
+      setIncorrectFormatEmail(false);
+    }
+
+    setEmail(value);
+  };
+
+  const changePassword = (value) => {
+    if (missingPassword) {
+      setMissingPassword(false);
+    }
+
+    setPassword(value);
+  };
+
+  const changeAddress1 = (value) => {
+    if (missingAddress1) {
+      setMissingAddress1(false);
+    }
+
+    setAddress1(value);
+  };
+
+  const changeAddress2 = (value) => {
+    if (missingAddress2) {
+      setMissingAddress2(false);
+    }
+
+    setAddress2(value);
+  };
+
+  const changePhone = (value) => {
+    if (missingPhone) {
+      setMissingPhone(false);
+    }
+
+    if (setIncorrectFormatPhone) {
+      setIncorrectFormatPhone(false);
+    }
+
+    setPhone(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let regExp =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (regExp.test(email)) {
+      console.log("correct format");
+    } else {
+      console.log("wrong format");
+      setIncorrectFormatEmail(true);
+    }
+
+    let regExp2 = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    if (regExp2.test(phone)) {
+      console.log("correct format");
+    } else {
+      console.log("wrong format");
+      setIncorrectFormatPhone(true);
+    }
+
+    return;
+  };
+
+  const handleWarnings = (e) => {
+    e.preventDefault();
+
+    if (!firstName) {
+      setMissingFirstName(true);
+    }
+
+    if (!lastName) {
+      setMissingLastName(true);
+    }
+
+    if (!email) {
+      setMissingEmail(true);
+    }
+
+    if (!password) {
+      setMissingPassword(true);
+    }
+
+    if (!address1) {
+      setMissingAddress1(true);
+    }
+
+    if (!address2) {
+      setMissingAddress2(true);
+    }
+
+    if (!phone) {
+      setMissingPhone(true);
+    }
+
+    if (!username) {
+      setMissingUsername(true);
+    }
+  };
 
   return (
     <motion.div
@@ -46,105 +210,265 @@ export default function CreateAccount() {
           </div>
 
           <div className={styles.left4thContainer}>
-            <div className={styles.nameInputContainer}>
+            <div
+              className={
+                missingFirstName
+                  ? styles.nameInputContainerRed
+                  : styles.nameInputContainer
+              }
+            >
               <FormField
                 type="text"
                 standard="labeleffect"
-                value={"firstName"}
+                value={firstName}
                 keys={"firstName"}
                 className={styles.nameInput}
                 effect={"effect_9"}
-                handleOnChange={(value) => console.log(value)}
+                handleOnChange={(value) => changeFirstName(value)}
                 placeholder={"First Name"}
               />
+              {missingFirstName && (
+                <span className={styles.requiredLabel}>
+                  First Name is required.
+                </span>
+              )}
             </div>
 
-            <div className={styles.nameInputContainer}>
+            <div
+              className={
+                missingLastName
+                  ? styles.nameInputContainerRed
+                  : styles.nameInputContainer
+              }
+            >
               <FormField
                 type="text"
                 standard="labeleffect"
-                value={"lastName"}
+                value={lastName}
                 keys={"lastName"}
                 className={styles.nameInput}
                 effect={"effect_9"}
-                handleOnChange={(value) => console.log(value)}
+                handleOnChange={(value) => changeLastName(value)}
                 placeholder={"Last Name"}
               />
+              {missingLastName && (
+                <span className={styles.requiredLabel}>
+                  Last Name is required.
+                </span>
+              )}
             </div>
           </div>
 
           <div className={styles.left5thContainer}>
-            <div className={styles.emailInputContainer}>
+            <div
+              className={
+                missingUsername
+                  ? styles.emailInputContainerRed
+                  : styles.emailInputContainer
+              }
+            >
               <FormField
                 type="text"
                 standard="labeleffect"
-                value={"email"}
+                value={username}
+                keys={"username"}
+                className={styles.nameInput}
+                effect={"effect_9"}
+                handleOnChange={(value) => changeUsername(value)}
+                placeholder={"Username"}
+              />
+              {missingUsername && (
+                <span className={styles.requiredLabel}>
+                  Username is required.
+                </span>
+              )}
+            </div>
+          </div>
+
+          <div className={styles.left5thContainer}>
+            <div
+              className={
+                missingEmail
+                  ? styles.emailInputContainerRed
+                  : styles.emailInputContainer
+              }
+            >
+              <FormField
+                type="text"
+                standard="labeleffect"
+                value={email}
                 keys={"email"}
                 className={styles.nameInput}
                 effect={"effect_9"}
-                handleOnChange={(value) => console.log(value)}
+                handleOnChange={(value) => changeEmail(value)}
                 placeholder={"Email"}
               />
+              {incorrectFormatEmail && (
+                <span className={styles.requiredLabel}>
+                  Please format your email: email@domain.com
+                </span>
+              )}
+              {missingEmail && (
+                <span className={styles.requiredLabel}>
+                  Please enter a valid email address.
+                </span>
+              )}
             </div>
           </div>
 
           <div className={styles.left14thContainer}>
-            <div className={styles.emailInputContainer}>
+            <div
+              className={
+                missingPassword
+                  ? styles.emailInputContainerRed
+                  : styles.emailInputContainer
+              }
+            >
               <FormField
-                type="password"
+                type={inputType}
                 standard="labeleffect"
-                value={"password"}
+                value={password}
                 keys={"password"}
                 className={styles.nameInput}
                 effect={"effect_9"}
-                handleOnChange={(value) => console.log(value)}
+                handleOnChange={(value) => changePassword(value)}
                 placeholder={"Password"}
               />
+
+              {missingPassword && (
+                <span className={styles.requiredLabel}>
+                  Please enter a valid password.
+                </span>
+              )}
+
+              {inputType === "password" && (
+                <div
+                  onClick={(e) => showPassword(e)}
+                  className={styles.revealPasswordIconContainer}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+              )}
+
+              {inputType === "text" && (
+                <div
+                  onClick={(e) => hidePassword(e)}
+                  className={styles.revealPasswordIconContainer}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
+                      clipRule="evenodd"
+                    />
+                    <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
+                  </svg>
+                </div>
+              )}
             </div>
           </div>
 
           <div className={styles.left6thContainer}>
-            <div className={styles.emailInputContainer}>
+            <div
+              className={
+                missingAddress1
+                  ? styles.emailInputContainerRed
+                  : styles.emailInputContainer
+              }
+            >
               <FormField
                 type="text"
                 standard="labeleffect"
-                value={"streetAddress"}
+                value={address1}
                 keys={"address1"}
                 className={styles.nameInput}
                 effect={"effect_9"}
-                handleOnChange={(value) => console.log(value)}
+                handleOnChange={(value) => changeAddress1(value)}
                 placeholder={"Street Address"}
               />
+
+              {missingAddress1 && (
+                <span className={styles.requiredLabel}>
+                  Please enter a valid address.
+                </span>
+              )}
             </div>
           </div>
 
           <div className={styles.left7thContainer}>
-            <div className={styles.emailInputContainer}>
+            <div
+              className={
+                missingAddress2
+                  ? styles.emailInputContainerRed
+                  : styles.emailInputContainer
+              }
+            >
               <FormField
                 type="text"
                 standard="labeleffect"
-                value={"cityStateZip"}
+                value={address2}
                 keys={"address2"}
                 className={styles.nameInput}
                 effect={"effect_9"}
-                handleOnChange={(value) => console.log(value)}
-                placeholder={"City, State, ZIP"}
+                handleOnChange={(value) => changeAddress2(value)}
+                placeholder={"City, State, Zip"}
               />
+
+              {missingAddress2 && (
+                <span className={styles.requiredLabel}>
+                  Please enter a valid address
+                </span>
+              )}
             </div>
           </div>
 
           <div className={styles.left8thContainer}>
-            <div className={styles.emailInputContainer}>
+            <div
+              className={
+                missingPhone
+                  ? styles.emailInputContainerRed
+                  : styles.emailInputContainer
+              }
+            >
               <FormField
                 type="text"
                 standard="labeleffect"
-                value={"phoneNumber"}
+                value={phone}
                 keys={"phone"}
                 className={styles.nameInput}
                 effect={"effect_9"}
-                handleOnChange={(value) => console.log(value)}
+                handleOnChange={(value) => changePhone(value)}
                 placeholder={"Phone Number"}
               />
+
+              {missingPhone && (
+                <span className={styles.requiredLabel}>
+                  Please enter a phone number.
+                </span>
+              )}
+
+              {incorrectFormatPhone && (
+                <span className={styles.requiredLabel}>
+                  Please enter a 10 digit phone number, ex. 888 123-4567
+                </span>
+              )}
             </div>
           </div>
 
@@ -161,7 +485,7 @@ export default function CreateAccount() {
                 <motion.div
                   transition={{ duration: 0.275 }}
                   animate={{
-                    scale: [1, 1.25, 1.5, 1.25, 1],
+                    scale: [0.5, 1, 1.5, 1, 1],
                     rotate: [0, 90, 180, 270, 360],
                   }}
                 >
@@ -199,7 +523,37 @@ export default function CreateAccount() {
             PowerUp Rewards <span>Terms & Conditions</span>.
           </div>
 
-          <div className={styles.left11thContainer}>SUBMIT</div>
+          {!(
+            !firstName ||
+            !lastName ||
+            !email ||
+            !password ||
+            !address2 ||
+            !address1 ||
+            !phone
+          ) && (
+            <div
+              onClick={(e) => handleSubmit(e)}
+              className={styles.left11thContainer}
+            >
+              SUBMIT
+            </div>
+          )}
+
+          {(!firstName ||
+            !lastName ||
+            !email ||
+            !password ||
+            !address2 ||
+            !address1 ||
+            !phone) && (
+            <div
+              onClick={(e) => handleWarnings(e)}
+              className={styles.left11thContainer2}
+            >
+              SUBMIT
+            </div>
+          )}
 
           <div className={styles.left12thContainer}>
             <div className={styles.divisorContainer}>
