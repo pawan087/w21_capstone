bd2ccd0c37bd7b15a381dc594e68b34ae17adc55
Pawan Chahal <chahal.pawanpreet@gmail.com>
2021-11-17 18:19:39 -0800

    fixed random bugs

--- backend/db/seeders/20201030163017-demo-user.js
+++ backend/db/seeders/20201030163017-demo-user.js
@@ -17,7 +17,7 @@ module.exports = {
           address1: "123 Main St.",
           address2: "San Jose, CA 95127",
           cart: [1, 4],
-          recentlyViewed: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+          recentlyViewed: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
         },
         {
           email: faker.internet.email(),
@@ -135,7 +135,7 @@ module.exports = {
           hashedPassword: bcrypt.hashSync(faker.internet.password()),
           firstName: faker.name.firstName(),
           lastName: faker.name.lastName(),
-          phone: String(faker.phone.phoneNumber()),
+          phone: "(408) 555-5555",
           address1: faker.address.streetAddress(),
           address2: `${faker.address.city()}, ${faker.address.state()}, ${faker.address.zipCode()}}`,
           cart: [2, 5],
