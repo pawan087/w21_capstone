cba7229a8fa10fe860de18fc3232f49dd024a578
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-01-03 18:53:56 -0800

    fixed password eye-reveal bug

--- frontend/src/components/CreateAccount/CreateAccount.jsx
+++ frontend/src/components/CreateAccount/CreateAccount.jsx
@@ -15,23 +15,14 @@ export default function CreateAccount() {
 
   const sessionUser = useSelector((state) => state.session.user);
 
-  const [firstName, setFirstName] = useState("Pawan");
-  const [lastName, setLastName] = useState("Chahal");
-  const [username, setUsername] = useState("pawan087");
-  const [email, setEmail] = useState("chahal.pawanpreet@gmail");
-  const [password, setPassword] = useState("Martinez61!");
-  const [address1, setAddress1] = useState("619 Davenport Dr.");
-  const [address2, setAddress2] = useState("San Jose, CA 95127");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
   const [phone, setPhone] = useState("");
-
-  // const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
-  // const [username, setUsername] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [address1, setAddress1] = useState("");
-  // const [address2, setAddress2] = useState("");
-  // const [phone, setPhone] = useState("");
 
   const [notRobot, setNotRobot] = useState(true);
   const [inputType, setInputType] = useState("password");
@@ -716,48 +707,6 @@ export default function CreateAccount() {
                   upper case letter, one lower case letter, one digit and one
                   special character.
                 </span>
-              )}
-
-              {inputType === "password" && (
-                <div
-                  onClick={(e) => showPassword(e)}
-                  className={styles.revealPasswordIconContainer}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                    <path
-                      fill-rule="evenodd"
-                      d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                </div>
-              )}
-
-              {inputType === "text" && (
-                <div
-                  onClick={(e) => hidePassword(e)}
-                  className={styles.revealPasswordIconContainer}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z"
-                      clipRule="evenodd"
-                    />
-                    <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
-                  </svg>
-                </div>
               )}
             </div>
           </div>
