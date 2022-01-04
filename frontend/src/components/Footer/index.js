b530b753f489e7621fee54bb7d6de4622d7ce963
Pawan Chahal <chahal.pawanpreet@gmail.com>
2022-01-03 16:10:19 -0800

    added linkedin info to footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -12,30 +12,41 @@ const Footer = () => {
   const x = useMotionValue(0);
   const y = useMotionValue(0);
 
+  const x2 = useMotionValue(0);
+  const y2 = useMotionValue(0);
+
   const rotateX = useTransform(y, [-100, 100], [60, -60]);
   const rotateY = useTransform(x, [-100, 100], [-60, 60]);
+
+  const rotateX2 = useTransform(y, [-100, 100], [60, -60]);
+  const rotateY2 = useTransform(x, [-100, 100], [-60, 60]);
 
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <div className={styles.footerTop}>
           <motion.div
-              dragTransition={{ bounceStiffness: 400, bounceDamping: 17.5 }}
+            dragTransition={{ bounceStiffness: 400, bounceDamping: 17.5 }}
+            style={{
+              x: x,
+              y: y,
+              rotateX: rotateX,
+              rotateY: rotateY,
+              cursor: "grab",
+            }}
+            drag
+            dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            dragElastic={0.6}
+            whileTap={{ cursor: "grabbing" }}
+            className={styles.gitIconContainer}
+          >
+            <img
               style={{
-                x: x,
-                y: y,
-                rotateX: rotateX,
-                rotateY: rotateY,
-                cursor: "grab",
+                pointerEvents: "none",
               }}
-              drag
-              dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
-              dragElastic={0.6}
-              whileTap={{ cursor: "grabbing" }} className={styles.gitIconContainer}>
-            <img       style={{
-              pointerEvents: "none",
-
-            }} src={githubImg} alt="Github Logo" />
+              src={githubImg}
+              alt="Github Logo"
+            />
           </motion.div>
 
           <div className={styles.gitLink}>
@@ -44,17 +55,49 @@ const Footer = () => {
               rel="noreferrer"
               href="https://github.com/pawan087"
               className={styles.link}
-
             >
-              Pawan Chahal
+              GitHub
             </a>
           </div>
 
-          
+          <motion.div
+            dragTransition={{ bounceStiffness: 400, bounceDamping: 17.5 }}
+            style={{
+              x: x2,
+              y: y2,
+              rotateX: rotateX2,
+              rotateY: rotateY2,
+              cursor: "grab",
+            }}
+            drag
+            dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            dragElastic={0.6}
+            whileTap={{ cursor: "grabbing" }}
+            className={styles.gitIconContainer}
+          >
+            <img
+              style={{
+                pointerEvents: "none",
+              }}
+              src={"https://raw.githubusercontent.com/MCE-Design/solo_project2/main/react-app/src/images/LI-In-Bug.png"}
+              alt="LinkedIn Logo"
+            />
+          </motion.div>
+
+          <div className={styles.gitLink}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/pawanchahal/"
+              className={styles.link}
+            >
+              LinkedIn
+            </a>
+          </div>
         </div>
 
         <div className={styles.footerBottom}>
-          <div className={styles.footerCopyright}>©2021 gameStopClone</div>
+          <div className={styles.footerCopyright}>©2021 Pawan Chahal</div>
         </div>
       </div>
     </footer>
